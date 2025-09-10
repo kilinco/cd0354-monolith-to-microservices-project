7971566d5cc85fc715294c06e70520b2951e1208
Osman <osmancihankilinc@gmail.com>
2025-09-10 00:47:26 -0700

    Refactor & containerize

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=myAdmin
+export POSTGRES_PASSWORD=HnumcRQrT3v_Phr
+export POSTGRES_HOST=postgres.cdni7asvbsxe.us-west-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_BUCKET=test-056805471142-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=k8s
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+
+echo "Set the credentials successfully!"
